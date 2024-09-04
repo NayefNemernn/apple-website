9614851938b6c19cc246dc0eb982f11ac1fd41c4
NayefNemernn <nayefnemernn@gmail.com>
2024-09-04 03:11:25 +0300

    Generating Hero, Highlights and VideoCarousel Components

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,35 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
 
 const Highlights = () => {
+
+    useGSAP(()=>{
+        gsap.to('#title', { opacity:1, y:0 })
+        gsap.to('.link', { opacity:1, y:0, duration:1, stagger: 0.25 })
+
+    })
+
   return (
-    <div>Highlights</div>
+    <section id="hightlights" className="w-screen overflow-hidden h-full 
+    common-padding bg-zinc">
+        <div className="screen-max-width">
+            <div className="mb-12 w-full items-end justify-between md:flex">
+                <h1 id="title" className="section-heading ">Get the Highlights.</h1>
+                <div className="flex flex-wrap items-end gap-5">
+                    <p className="link">
+                        Watch the Film
+                        <img src={watchImg} alt="watch" className="ml-2 " />
+                    </p>
+                    <p className="link">
+                        Watch the Event
+                        <img src={rightImg} alt="watch" className="ml-2 " />
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    </section>
   )
 }
 
