2b49e87f8131a93dd6a1eef19a1bb41571ec73a2
NayefNemernn <nayefnemernn@gmail.com>
2024-09-10 13:37:40 +0300

    creating Model Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import Model from "./components/Model"
 
 function App() {
-  
+
 
   return (
     <main className="bg-balck">
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
